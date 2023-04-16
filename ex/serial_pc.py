bdab130962e2b284fbc0e2ce8b852e6ed1c99bf5
Charlie H. Shin <hwan.shin@gmail.com>
2023-04-17 03:53:59 +0900

    serial data example

--- ex/serial_pc.py
+++ ex/serial_pc.py
@@ -9,17 +9,23 @@ def init(port):
 
 
 def main():
+    # read two bytes of data
+    ser = init('/dev/ttyUSB0')
     while True:
-        # read two bytes of data
-        data = ser.read(8)
+        print("Sending data to Pico: 1234567890")
+        ser.write(b'1234567890')
+        #data = ser.read(8)
         #data = ser.readline()
         # convert bytestring  to unicode transformation format -8 bit
-        data_read = str(data).encode("utf-8")
+        #data_read = str(data).encode("utf-8")
         #print("Pico's Core Temperature : " + temperature + " Degree Celcius")
-        print(f'data_read: {data_read}')
-        time.sleep(0.2)
-        ser.write(b'm1234567890')
-        time.sleep(0.2)
+        #print(f'data_read: {data_read}')
+        #time.sleep(0.2)
+        #:w
+        #ser.write(b'm1234567890')
+        data=ser.read(10)
+        print(f'data: {data}')
+        time.sleep(0.8)
 
 
 if __name__ == "__main__":
