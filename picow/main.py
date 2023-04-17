ba1e32c56a1bae574ef7229b396b5d0abf12b4e9
Charlie H. Shin <hwan.shin@gmail.com>
2023-04-18 00:18:34 +0900

    common read/write main.py for pico-w

--- picow/main.py
+++ picow/main.py
@@ -4,6 +4,8 @@ import ubinascii
 
 import time
 
+SERIAL_TIMEOUT = 5 # ms
+
 led = Pin('LED', Pin.OUT)
 led_red = Pin(15, Pin.OUT)
 led_green = Pin(14, Pin.OUT)
@@ -14,46 +16,58 @@ print('Starting script')
 
 def init_serial():
     uart0 = UART(0, tx=Pin(0), rx=Pin(1))
-    uart0.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=10)
-    
-    uart1 = UART(1, tx=Pin(6), rx=Pin(7))
-    uart1.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=10)
-    
+    uart0.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=SERIAL_TIMEOUT)
+
+    uart1 = UART(1, tx=Pin(4), rx=Pin(5))
+    uart1.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=SERIAL_TIMEOUT)
+
     return uart0, uart1
 
 def main():
     led.on()
-    u0, u1 = init_serial()
-    buffer = b''
+    led_on_green()
+    spc, seth = init_serial()
+    buf2eth = b''
+    buf2pc = b''
+
     while True:
-        data = u0.read()
-        if data:
-            led_on_green()
-            led_off_red()
-            buffer += data
+        data2eth = spc.read()
+        if data2eth:
+            led_blink_red()
+            buf2eth += data2eth
         else:
-            n_received = len(buffer)
-            if not n_received:
-                continue
-            else:
-                print(n_received), " characters received")
-                print(buffer)
-                n_write = n_received
+            if buf2eth:
+                n_to_write = len(buf2eth)
                 while True:
-                    n_wrtten = u1.write(buffer)
+                    n_written = seth.write(buf2eth)
                     if not n_written:
                         print("Error write to uart1")
                         break
-                    elif n_written < n_write:
-                        buffer = buffer[n_written:]
-                        n_write -= n_written
+                    elif n_written < n_to_write:
+                        buf2eth = buf2eth[n_written:]
+                        n_to_write -= n_written
                     else:
                         break
-                buffer = b''
-                    
-                        
-                    
-                buffer = b''
+                buf2eth = b''
+            else:
+                data2pc = seth.read()
+                if data2pc:
+                    led_blink_red()
+                    buf2pc += data2pc
+                else:
+                    if buf2pc:
+                        n_to_write = len(buf2pc)
+                        while True:
+                            n_written = spc.write(buf2pc)
+                            if not n_written:
+                                print("Error write to uart0")
+                                break
+                            elif n_written < n_to_write:
+                                buf2pc = buf2pc[n_written:]
+                                n_to_write -= n_written
+                            else:
+                                break
+                        buf2pc = b''
 
     while 0:
         #b64 = conn.recv(1024)
@@ -67,17 +81,17 @@ def main():
         led_blink_green()
         led_blink_red()
 
-def blink_red(timer):
+def blink_red(t):
     led_red.toggle()
 
-def blink_green(timer):
+def blink_green(t):
     led_green.toggle()
 
 def led_blink_red():
-    timer_red.init(freq=1.5, mode=Timer.PERIODIC, callback=blink_red)
+    timer_red.init(freq=10, mode=Timer.PERIODIC, callback=blink_red)
 
 def led_blink_green():
-    timer_red.init(freq=1.5, mode=Timer.PERIODIC, callback=blink_green)
+    timer_red.init(freq=10, mode=Timer.PERIODIC, callback=blink_green)
 
 def led_on_red():
     timer_red.deinit()
@@ -94,6 +108,6 @@ def led_off_red():
 def led_off_green():
     timer_red.deinit()
     led_green.off()
-    
+
 if __name__ == '__main__':
     main()
