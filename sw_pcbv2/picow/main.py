3c52485e056cabca3d8ff119385966f2c6aa21f0
Charlie H. Shin <hwan.shin@gmail.com>
2023-04-30 07:09:20 +0900

    main.py update

--- sw_pcbv2/picow/main.py
+++ sw_pcbv2/picow/main.py
@@ -1,73 +1,154 @@
+'''pico-w'''
 from machine import Pin
 from machine import Timer
 from machine import UART
 import ubinascii
 
 import utime
+
+from phew import logging, server, access_point, dns
+from phew.template import render_template
+from phew.server import redirect
+
 import utils
 
-SERIAL_TIMEOUT = 100 # ms
+DOMAIN = "Winners.hotspot"  # This is the address that is shown on the Captive Portal
+
+SERIAL1_TIMEOUT = 20 # ms
+UART1_DELAY = 0.05 # 50ms
 
 led = Pin('LED', Pin.OUT)
+
+interrupt_flag = 0
+
+def callback(btn):
+    global interrupt_flag
+    interrupt_flag=1
+
+    ap = access_point("위너스 핫스팟")
+    ip = ap.ifconfig()[0] # Grab the IP address and store it
+    logging.info(f"starting DNS server on {ip}")
+    dns.run_catchall(ip)  # Catch all requests and reroute them
+    server.run()          # Run the server
+    logging.info("Webserver Started")    
+
+print('Starting script')
+
+@server.route("/", methods=['GET'])
+def index(request):
+    """ Render the Index page"""
+    if request.method == 'GET':
+        logging.debug("Get request")
+        return render_template("index.html")
+
+# microsoft windows redirects
+@server.route("/ncsi.txt", methods=["GET"])
+def hotspot(request):
+    print(request)
+    print("ncsi.txt")
+    return "", 200
+
+
+@server.route("/connecttest.txt", methods=["GET"])
+def hotspot(request):
+    print(request)
+    print("connecttest.txt")
+    return "", 200
+
+
+@server.route("/redirect", methods=["GET"])
+def hotspot(request):
+    print(request)
+    print("****************ms redir*********************")
+    return redirect(f"http://{DOMAIN}/", 302)
+
+# android redirects
+@server.route("/generate_204", methods=["GET"])
+def hotspot(request):
+    print(request)
+    print("******generate_204********")
+    return redirect(f"http://{DOMAIN}/", 302)
+
+# apple redir
+@server.route("/hotspot-detect.html", methods=["GET"])
+def hotspot(request):
+    print(request)
+    """ Redirect to the Index Page """
+    return render_template("index.html")
+
+
+@server.catchall()
+def catch_all(request):
+    print("***************CATCHALL***********************\n" + str(request))
+    return redirect("http://" + DOMAIN + "/")
 
 
 def init_serial():
     uart1 = UART(1, tx=Pin(4), rx=Pin(5))
-    uart1.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=SERIAL_TIMEOUT)
+    uart1.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=SERIAL1_TIMEOUT)
 
     return uart1
 
-def main():    
-    s1 = init_serial()
-
-    json_settings = utils.load_json_settings()
-    print(json_settings)
-    len_json = len(json_settings)
-    len_json = bytes(f'{len_json}\n', 'utf-8')
+def send_settings(sp, json_settings):
+    len_json = bytes(f'{len(json_settings)}\n', 'utf-8')
     print(len_json)
 
     while True:
-        srecv = s1.readline()
+        got_ready = False
         while True:
+            srecv = sp.readline()
             print(srecv)
-            if srecv and srecv.decode('utf-8')=='READY':
+            if srecv is None and got_ready:
                 break
-            srecv = s1.readline()
+            try:
+                ready = srecv.decode('utf-8')
+                if ready=='READY_1\n':
+                    got_ready = True
+            except Exception as e:
+                print(e)
 
-        print('PEER READY')
+        print('PEER READY_1')
+        
+        sp.write(len_json)
+        utime.sleep(UART1_DELAY)
 
-        s1.write(len_json)
-
-        result = True
-        srecv = s1.readline()
+        restart = True
+        got_ready = False
         while True:
+            srecv = sp.readline()
             print(srecv)
-            if srecv:
-                if srecv.decode('utf-8')=='READY':
+            if srecv is None and got_ready:
+                restart = False
+                break
+            try:
+                ready = srecv.decode('utf-8')
+                if ready=='READY_2\n':
+                    got_ready = True
+                elif ready=='RESTART\n':
+                    restart = True
                     break
-                elif srecv.decode('utf-8')=='FAIL':
-                    result=False
-                    break
-            srecv = s1.readline()
+            except Exception as e:
+                print(e)
 
-        if not result:
+        if restart:
             continue
-        print('PEER READY 2')
 
-        nw = s1.write(json_settings)
+        print('PEER READY2')
+
+        nw = sp.write(json_settings)
         print(f'written: {nw}')
-
-        srecv = s1.readline()
-        print(srecv)
-        while True:
-            if srecv and srecv.decode('utf-8')=='READY':
-                break
-            srecv = s1.readline()
-            print(srecv)
-
-        print('PEER READY')
-
+        break
+    
+def main():
     led.on()
+    s1 = init_serial()
+    json_settings = utils.load_json_settings()
+    print(json_settings)
+
+    send_settings(s1, json_settings)
+    led.on()
+
+    callback2(0)
 
     return
 
--- sw_pcbv2/w5500/main.py
+++ sw_pcbv2/w5500/main.py
@@ -1,6 +1,7 @@
 '''
 W5500 
 '''
+import machine
 from machine import Pin
 from machine import Timer
 from machine import UART
@@ -17,7 +18,8 @@ default_led_timer = Timer()
 
 print('Starting W5500 script')
 
-SERIAL1_TIMEOUT = 500
+SERIAL1_TIMEOUT = 20 # ms
+UART1_DELAY = 0.05 # 50ms
 
 def init_serial():
     uart1 = UART(1, tx=Pin(4), rx=Pin(5))
@@ -25,34 +27,44 @@ def init_serial():
     
     return uart1
 
+def receive_settings(sp):
+    while True:
+        sp.write(bytes('READY_1\n', 'utf-8'))
+        utime.sleep(UART1_DELAY*2)
+        srecv = sp.readline()
+        print(srecv)
+        try:
+            len_settings = int(srecv.decode('utf-8').strip())
+            break
+        except Exception as e:
+            print(e)
+
+    print(f'len_settings:{len_settings}')
+
+    while True:
+        sp.write(bytes('READY_2\n', 'utf-8'))
+        utime.sleep(UART1_DELAY*2)
+        srecv = sp.read(len_settings)
+        print(srecv)
+        try:
+            json_settings = srecv.decode('utf-8')
+            break
+        except Exception as e:
+            print(e)
+
+    print(json_settings)
+    return json_settings
+
 def main():
     s1 = init_serial()
+    json_settings = receive_settings(s1)
+    settings = ujson.loads(json_settings)
+    print(settings)
     default_led.on()
+    print('resetting...')
+    utime.sleep(0.5)
+    machine.reset()
     
-    s1.write(bytes('READY','utf-8'))
-    len_json = b''
-    rc = s1.read(1)
-    print(rc)
-    while True:
-        if rc:
-            if rc.decode('utf-8') == '\n':
-                break
-        if rc and rc !=b'\x00':
-            len_json += rc
-        rc = s1.read(1)
-        print(rc)
-    
-    print(f'len_json: {len_json}')
-    len_json = len_json.decode('utf-8')
-    print(f'len_json: {len_json}')
-    
-    s1.write(bytes('READY','utf-8'))
-    
-    json_data = s1.read(int(len_json))
-    print(f'json_data: {json_data}')
-    json_data = json_data.decode('utf-8')
-    data = ujson.loads(json_data)
-    print(f'data: {data}')
 
 if __name__ == '__main__':
     main()
