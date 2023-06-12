5c47bbe00ed068014dd158ed8ace224e00d8a866
Charlie H. Shin <hwan.shin@gmail.com>
2023-06-13 01:17:31 +0900

    update

--- sw_wired/w5500/main-1.py
+++ sw_wired/w5500/main-1.py
@@ -53,10 +53,14 @@ def serial_thread_func():
                 print(f'sm[-7:]: {sm[-7:]}')
                 if cmd=='CNF_REQ':
                     json_settings = utils.load_json_settings()
-                    print(len(json_settings), ' bytes : ', json_settings)
-                    msg = bytes('CNF_JSN', 'utf-8') + bytes(json_settings, 'utf-8') + bytes('CNF_END\n', 'utf-8')
+                    print('json_settings: ', json_settings)
+                    print('type(json_settings): ', type(json_settings))
+                    settings = ujson.dumps(json_settings)
+                    print(len(settings), ' bytes : ', settings)
+                    msg = bytes('CNF_JSN', 'utf-8') + bytes(settings, 'utf-8') + bytes('CNF_END\n', 'utf-8')
                     u0.write(msg)
                 elif cmd=='CNF_WRT' and sm[-7:]=='CNF_END':
+                    u0.deinit()
                     settings = sm[7:-7]
                     print(f'Received settings: {settings}')
                     utils.save_settings(settings)
@@ -68,58 +72,34 @@ def serial_thread_func():
                 print(e)
         else:
             print(sm)
-            utime.sleep(0.2)
+            utime.sleep(0.5)
             
-    u0.deinit()
+def run_server(ip, port):
 
-def run_server(ip, port)
-
-    server_sock = socket(network.AF_INET, network.SOCK_STREAM)
-    server_sock.bind((ip, port))
+    server_sock = socket()#network.AF_INET, network.SOCK_STREAM)
+    server_sock.bind((ip, int(port)))
     
+    print('Listening on socket: ', server_sock)
+    server_sock.listen(3)
+
     while True:
-        print('Listening on socket: ', server_sock)
-        server_sock.listen(3)
         print('Waiting for connection...')
         conn, addr = server_sock.accept()
         print('Connected by ', conn, ' from ', addr)
-        b64 = conn.recv(32)
+        b64 = conn.recv(64)
         print('data received: ', b64)
         print('type(b64): ', type(b64))
-        if addr != settings['remote_ip']:
-            print('Not from remote')
-            remote_sock = socket()
-            print(f"connecting to remote {settings['remote_ip']}:{settings['remote_port']}")
-            #remote_sock.connect((settings['remote_ip'], settings['remote_port']))
-            remote_sock.connect(('192.168.0.254', 2004))
-            print(f"send {b64} to remote")
-            remote_sock.send(b64)
-            client_sock = conn
-            client_addr = addr
-            print("waiting for response from remote")
-            b64 = remote_sock.recv(32)
-            print(f"received {b64} from remote")
-            remote_sock.close()
-            client_sock.send(b64)
-            print(f"sent {b64} to client")
-            client_sock.close()
-        else:
-            print('From remote')
-            remote_sock = conn
-            remote_addr = addr
-            plc_sock = socket()
-            print(f"connecting to plc {settings['plc_ip']}:{settings['plc_port']}")
-            plc_sock.connect((settings['plc_ip'], settings['plc_port']))
-            print(f"send {b64} to plc")
-            plc_sock.send(b64)
-            print("waiting for response from plc")
-            b64 = plc_sock.recv(32)
-            print(f"received {b64} from plc")
-            plc_sock.close()
-            remote_sock.send(b64)
-            print(f"sent {b64} to remote")
-            remote_sock.close()
-    
+
+        if len(b64) > 4:
+            msg = b64.decode('utf-8')
+            cmd, msg = msg[:4], msg[4:]
+            print(f'cmd: {cmd}')
+
+            if cmd == 'TEXT':
+                print(f'msg: {msg}')
+            elif cmd == 'CIPH':
+                print(f'msg: {msg}')
+
 def main():
     global serial_thread_running
     
@@ -141,8 +121,11 @@ def main():
         json_settings['subnet'] = net_info[1]
         json_settings['gateway'] = net_info[2]
         utils.save_settings(ujson.dumps(json_settings))
-        run_server(json_settings['ip'], json_settings['port'])
+
         led_onoff(green, True)
+
+        run_server(json_settings['ip'], json_settings['port'])
+
         
     else:
         print('No IP assigned')
