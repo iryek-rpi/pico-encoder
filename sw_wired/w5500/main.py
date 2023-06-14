e67e0f24c824ee97fb91e3c9e7ccc1d09cac4b03
Charlie H. Shin <hwan.shin@gmail.com>
2023-06-15 02:27:32 +0900

    update

--- sw_wired/w5500/main.py
+++ sw_wired/w5500/main.py
@@ -8,15 +8,22 @@ from machine import UART
 
 import utime
 from usocket import socket
+import uselect
 import _thread
+import gc
 
 import network
 import ubinascii
 import ujson
+import mpyaes as aes
+import usocket
 
+import coder
 from led import *
 from w5500 import w5x00_init
 import utils
+
+KEY = b'aD\xd8\x11e\xdcy`\t\xdc\xe4\xa7\x1f\x11\x94\x93'
 
 print('Starting W5500 script')
 led_onoff(led, False)
@@ -27,7 +34,17 @@ led_onoff(red, True)
 SERIAL1_TIMEOUT = 300 # ms
 UART1_DELAY = 0.05 # 50ms
 
-serial_thread_status = True
+serial_thread_running = False
+
+btn = Pin(9, Pin.IN, Pin.PULL_UP)
+
+def btn_callback(btn):
+    global serial_thread_running
+    led_onoff(yellow, True)
+    print('Button pressed')
+    serial_thread_running = False
+
+btn.irq(trigger=Pin.IRQ_FALLING, handler=btn_callback)
 
 def init_serial():
     uart0 = UART(0, tx=Pin(0), rx=Pin(1))
@@ -35,197 +52,179 @@ def init_serial():
 
     return uart0
 
-def receive_settings(sp):
-    while True:
-        sp.write(bytes('READY_1\n', 'utf-8'))
-        utime.sleep(UART1_DELAY*2)
-        srecv = sp.readline()
-        print(srecv)
-        try:
-            len_settings = int(srecv.decode('utf-8').strip())
-            break
-        except Exception as e:
-            print(e)
-
-    print(f'len_settings:{len_settings}')
-
-    while True:
-        sp.write(bytes('READY_2\n', 'utf-8'))
-        utime.sleep(UART1_DELAY*2)
-        srecv = sp.read(len_settings)
-        print(srecv)
-        try:
-            json_settings = srecv.decode('utf-8')
-            break
-        except Exception as e:
-            print(e)
-
-    print(json_settings)
-    return json_settings
-
-def serial_thread():
+def serial_thread_func():
+    global serial_thread_running
     u0 = init_serial()
 
-    while serial_thread_status:
+    print('Serial Thread starting...')
+    
+    while serial_thread_running:
         sm = u0.readline()
         if sm:
             try:
                 sm = sm.decode('utf-8')
+                sm = sm.strip()
                 print(sm)
                 cmd = sm[:7]
+                print(f'cmd: {cmd}')
+                print(f'sm[-7:]: {sm[-7:]}')
                 if cmd=='CNF_REQ':
                     json_settings = utils.load_json_settings()
-                    print(f'{len(json_settings)}bytes : {json_settings}')
-                    msg = bytes(f'CNF_JSN{json_settings}CNF_END', 'utf-8')
+                    print('json_settings: ', json_settings)
+                    print('type(json_settings): ', type(json_settings))
+                    settings = ujson.dumps(json_settings)
+                    print(len(settings), ' bytes : ', settings)
+                    msg = bytes('CNF_JSN', 'utf-8') + bytes(settings, 'utf-8') + bytes('CNF_END\n', 'utf-8')
                     u0.write(msg)
-                elif cmd=='CONFSND' and sm[-7:]=='CONFEND':
-                    json_settings = sm[7:-7]
-                    print(json_settings)
-                    settings = ujson.loads(json_settings)
-                    print(settings)
+                elif cmd=='CNF_WRT' and sm[-7:]=='CNF_END':
+                    u0.deinit()
+                    settings = sm[7:-7]
+                    print(f'Received settings: {settings}')
+                    utils.save_settings(settings)
+                    serial_thread_running = False
+                    break
                 else:
                     print('Unknown command')
             except Exception as e:
                 print(e)
-    u0.deinit()
+        else:
+            print(sm)
+            utime.sleep(0.5)
+    print('Exit from UART thread')
 
-def send_settings(sp, json_settings):
-    len_json = bytes(f'{len(json_settings)}\n', 'utf-8')
-    print(len_json)
+def run_server(ip, port, key):
+    global serial_thread_running
 
-    while True:
-        nw = sp.write(json_settings)
-        sp.flush()
-        print(f'written: {nw}')
-        utime.sleep(1)
-        #nw = sp.write(bytes('hello','utf-8'))
+    server_sock = socket()#network.AF_INET, network.SOCK_STREAM)
+    #server_sock.setsockopt(usocket.SOL_SOCKET, usocket.SO_REUSEADDR, 1)
 
-        srecv = sp.readline()
-        print(srecv)
-        if srecv:
-            try:
-                ready = srecv.decode('utf-8')
-                if ready=='READY_0\n':
-                    break
-            except Exception as e:
-                print(e)
-        utime.sleep(0.05)
+    server_sock.bind((ip, int(port)))
     
+    print('Listening on socket: ', server_sock)
+    server_sock.listen(3)
+
+    if len(key) > len(coder.KEY):
+        key = key[:len(coder.KEY)]
+    keyb = key.encode()
+    keyb = keyb + coder.KEY[len(keyb):]
+
+    p1 = uselect.poll()
+    p1.register(server_sock, uselect.POLLIN)
+
+    print('Waiting for connection...')
+    conn = None
+    while serial_thread_running:
+        res = p1.poll(500)
+        if not res:
+            print('no client at server_sock')
+            continue
+        print('client available')
+        conn, addr = server_sock.accept()
+        print('Connected by ', conn, ' from ', addr)
+        break
+
+    if not serial_thread_running:
+        if conn:
+            conn.close()
+        server_sock.close()
+        return
+        
+    poller = uselect.poll()
+    poller.register(conn, uselect.POLLIN)
+    
+    while serial_thread_running:
+        res = poller.poll(500)
+        if not res:
+            print('no data from conn')
+            continue
+        print('data arrived at conn')
+    #while serial_thread_running:
+        b64 = conn.recv(128)
+        print('data received: ', b64)
+        print('type(b64): ', type(b64))
+        
+        if len(b64) <= 4:
+            print('irregular data. Exit')
+            serial_thread_running=False
+            utime.sleep(2)
+        else:
+            #msg = b64.decode('utf-8')
+            #cmd, msg = msg[:4], msg[4:]
+            cmd, msg = b64[:4], b64[4:]
+            cmd = cmd.decode('utf-8')
+            print(f'cmd: {cmd}')
+
+            if cmd == 'TEXT':
+                print(f'msg: {msg}')
+                IV = aes.generate_IV(16)
+                cipher = aes.new(keyb, aes.MODE_CBC, IV)
+                #msg = cipher.encrypt(msg.encode())
+                msg = cipher.encrypt(msg)
+                iv_c = IV + msg
+                print('IV: ')
+                print(IV)
+                print('msg: ')
+                print(msg)
+                conn.send(iv_c)
+            elif cmd == 'CIPH':
+                print(f'msg: {msg}')
+                IV, msg = msg[:16], msg[16:]
+                print('IV: ')
+                print(IV)
+                print('msg: ')
+                print(msg)
+                msga = bytearray(msg)
+                cipher = aes.new(keyb, aes.MODE_CBC, IV)
+                plaintext = cipher.decrypt(msga)
+                conn.send(plaintext)
+
+    serial_thread_running = False
+    
+    if conn:
+        conn.close()
+    if server_sock:
+        server_sock.close()
+
+    utime.sleep(2)
+
+    print('Resetting the machine')
+    machine.reset()
+
 def main():
-    global serial_thread_status
-    
-    _thread.start_new_thread(serial_thread, ())
+    global serial_thread_running
 
     json_settings = utils.load_json_settings()
     print(json_settings)
 
-    #s0 = init_serial()
-    #json_settings = receive_settings(s1)
-    #settings = ujson.loads(json_settings)
-    #settings['mode'] = 'ETH' #'SER"
-    #print(settings)
+    utime.sleep(1)
+    serial_thread_running = True
+    serial_thread = _thread.start_new_thread(serial_thread_func, ())
+    
+    if json_settings['dhcp']:
+        net_info = w5x00_init(None)
+    else:
+        net_info = w5x00_init((json_settings['ip'], json_settings['subnet'], json_settings['gateway']))
 
-    #send_settings(s0, json_settings)
-    ip = w5x00_init('192.168.2.13')
-    if ip:
-        print(f'IP assigned: {ip}')
-        blue.on()
+    if net_info[0]:
+        print('IP assigned: ', net_info[0])
+        json_settings['ip'] = net_info[0]
+        json_settings['subnet'] = net_info[1]
+        json_settings['gateway'] = net_info[2]
+        utils.save_settings(ujson.dumps(json_settings))
+
+        led_onoff(green, True)
+
+        run_server(json_settings['ip'], json_settings['port'], json_settings['key'])
+        
     else:
         print('No IP assigned')
-        led.off()
+        led_onoff(yellow, True)
 
     serial_thread_status = False
-    usleep(3)
-    print("Exit from main thread")
-
-temp = '''
-
-    ip = w5x00_init(settings['ip'])
-
-    server_sock = socket() #network.AF_INET, network.SOCK_STREAM)
-    server_sock.bind((ip, settings['port']))
-
-    #client_sock = socket(network.AF_INET, network.SOCK_STREAM)
-    red_led.off()
+    print("Waiting for 2 sec")
+    utime.sleep(2)
     
-    if settings['mode'] == 'ETH':
-        while True:
-            server_sock.listen()
-            print('Listening on socket: ', server_sock)
-
-            conn, addr = server_sock.accept()
-            print('Connected by ', conn, ' from ', addr)
-            b64 = conn.recv(32)
-            print('data received: ', b64)
-            print('type(b64): ', type(b64))
-            if addr != settings['remote_ip']:
-                print('Not from remote')
-                remote_sock = socket()
-                print(f"connecting to remote {settings['remote_ip']}:{settings['remote_port']}")
-                #remote_sock.connect((settings['remote_ip'], settings['remote_port']))
-                remote_sock.connect(('192.168.0.254', 2004))
-                print(f"send {b64} to remote")
-                remote_sock.send(b64)
-                client_sock = conn
-                client_addr = addr
-                print("waiting for response from remote")
-                b64 = remote_sock.recv(32)
-                print(f"received {b64} from remote")
-                remote_sock.close()
-                client_sock.send(b64)
-                print(f"sent {b64} to client")
-                client_sock.close()
-            else:
-                print('From remote')
-                remote_sock = conn
-                remote_addr = addr
-                plc_sock = socket()
-                print(f"connecting to plc {settings['plc_ip']}:{settings['plc_port']}")
-                plc_sock.connect((settings['plc_ip'], settings['plc_port']))
-                print(f"send {b64} to plc")
-                plc_sock.send(b64)
-                print("waiting for response from plc")
-                b64 = plc_sock.recv(32)
-                print(f"received {b64} from plc")
-                plc_sock.close()
-                remote_sock.send(b64)
-                print(f"sent {b64} to remote")
-                remote_sock.close()
-    else:
-        while True:
-            server_sock.listen()
-            print('Listening on socket: ', server_sock)
-
-            conn, addr = server_sock.accept()
-            print('Connected by ', conn, ' from ', addr)
-            b64 = conn.recv(32)
-            print('data received: ', b64)
-            print('type(b64): ', type(b64))
-            if addr != settings['remote_ip']:
-                print('Not from remote')
-                remote_sock = socket()
-                print(f'connecting to remote {settings['remote_ip']}:{settings['remote_port']}')
-                remote_sock.connect((settings['remote_ip'], settings['remote_port']))
-                remote_sock.send(b64)
-                client_sock = conn
-                client_addr = addr
-                b64 = remote_sock.recv(32)
-                remote_sock.close()
-                client_sock.send(b64)
-                client_sock.close()
-            else:
-                print('From remote')
-                remote_sock = conn
-                remote_addr = addr
-                server_sock = socket()
-                print(f'connecting to server {settings['server_ip']}:{settings['server_port']}')
-                server_sock.connect((settings['server_ip'], settings['server_port']))
-                server_sock.send(b64)
-                b64 = server_sock.recv(32)
-                server_sock.close()
-                remote_sock.send(b64)
-                remote_sock.close()
-                '''
+    print("Exit from main thread")
 
 if __name__ == '__main__':
     main()
