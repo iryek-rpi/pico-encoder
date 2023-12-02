66c403f6befe3e51d6797323fd438f1d1676aa6b
Charlie H. Shin <hwan.shin@gmail.com>
2023-12-03 02:32:16 +0900

    change debug messages

--- sw/w5500/coder.py
+++ sw/w5500/coder.py
@@ -24,10 +24,8 @@ def fix_len_and_encode_key(key):
     return keyb + DEFAULT_LENGTH_KEY[len(keyb):]
 
 def encrypt_text(b64, fixed_binary_key, dest):
-    print('data received: ', b64)
-    print('type(b64): ', type(b64))
+    print('data received: ', b64, 'of type(b64): ', type(b64))
     
-    print('msg: ', b64)
     IV = aes.generate_IV(16)
     print('fixed_binary_key: ', fixed_binary_key, 'type(fixed_binary_key): ', type(fixed_binary_key))
     cipher = aes.new(fixed_binary_key, aes.MODE_CBC, IV)
@@ -35,6 +33,7 @@ def encrypt_text(b64, fixed_binary_key, dest):
     print('IV:', IV, ' msg:', msg)
     encrypted_msg = IV + msg
 
+    print(f'dest: {dest}')
     dest[1](encrypted_msg, dest[0])
     return encrypted_msg
 
