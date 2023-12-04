d069196643bcc0a9f5f16365a04aede3b28ba230
Hyunghwan Shin <hwan@gmail.com>
2023-12-04 18:44:57 +0900

    device to device socket read/write

--- sw/ex/async_relay/async-device/pico_async_device.py
+++ sw/ex/async_relay/async-device/pico_async_device.py
@@ -1,10 +1,10 @@
 import asyncio
 import socket
-import time
+import os
 
-HOST_IP = '127.0.0.1'
-HOST_PORT = 2004
-PEER_IP = '127.0.0.1'
+HOST_IP = os.environ['HOST_IP']
+HOST_PORT = os.environ['HOST_PORT']
+PEER_IP = os.environ['PEER_IP']
 CRYPTO_PORT = 8503
 TEXT_PORT = 2004
 
@@ -34,17 +34,12 @@ def send_tcp_data_sync(data, addr):
         data = data[sent:]
     sock.close()
 
-async def process_stream(handler, key, reader, writer, name, dest):
+async def process_stream(handler, reader, writer, name, dest):
     print(f'handling {name}..')
-    #b64 = await reader.readline()
     while True:
         b64 = await reader.read(100)
         addr = writer.get_extra_info('peername')
         print(f"Received {b64} from {addr}")
-        if len(b64)>0:
-            _ = handler(b64, key, dest)
-        else:
-            break
 
     print(f"Close the connection for handle_{name}()")
     writer.close()
@@ -52,15 +47,17 @@ async def process_stream(handler, key, reader, writer, name, dest):
     reader.close()
     await reader.wait_closed()
 
-async def handle_crypto(reader, writer):
-    print(f"\n### handle TCP CRYPTO from {reader} {writer}")
-    dest = ((HOST_IP, int(HOST_PORT)), send_tcp_data_sync)
-    await process_stream(decrypt_crypto, reader, writer, 'CRYPTO', dest)
-
 async def handle_tcp_text(reader, writer):
     print(f"\n### handle TCP TEXT from {reader} {writer}")
-    dest = ((PEER_IP, CRYPTO_PORT), send_tcp_data_sync)
+    #dest = ((PEER_IP, CRYPTO_PORT), send_tcp_data_sync)
+    dest = (PEER_IP, CRYPTO_PORT)
     await process_stream(encrypt_text, reader, writer, 'TEXT', dest)
+
+async def handle_crypto(reader, writer):
+    print(f"\n### handle TCP CRYPTO from {reader} {writer}")
+    #dest = ((HOST_IP, int(HOST_PORT)), send_tcp_data_sync)
+    dest = (HOST_IP, HOST_PORT)
+    await process_stream(decrypt_crypto, reader, writer, 'CRYPTO', dest)
 
 def main():
     loop = asyncio.get_event_loop()
