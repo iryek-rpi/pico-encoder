93eb2fea7f68b131239b79cc45b702f28ace0c6e
Charlie H. Shin <hwan.shin@gmail.com>
2023-12-17 22:30:57 +0900

    device #11, #12 and update w5500.py

--- firmware/mac/w5500.py
+++ firmware/mac/w5500.py
@@ -9,10 +9,26 @@ import time
 def get_nic():
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
-    
-    #nic.active(True)
     return nic
 
+def get_mac():
+    nic = get_nic()
+    m = nic.config('mac')
+    h = m.hex()
+    print(f'mac: {m} {h}')
+    return m, h
+
+def save_mac():
+    m, h = get_mac()
+    with open('mac.bin', 'wb') as f:
+        f.write(m)
+        
+def read_mac():
+    with open('mac.bin', 'rb') as f:
+        m = f.read()
+        h = m.hex()
+        return m, h
+    
 def w5x00_init(net_config):
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
