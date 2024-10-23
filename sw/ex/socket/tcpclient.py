73b2d2d08907feecf68745453ce9fe2156fc0a44
Charlie H. Shin <hwan.shin@gmail.com>
2024-10-24 00:57:47 +0900

    xor test code

--- sw/ex/socket/tcpclient.py
+++ sw/ex/socket/tcpclient.py
@@ -1,18 +1,30 @@
 from socket import *
+import xor
 
-ip = "192.168.1.10"
-port = 5002
+MSG = "123456"
+KEY = 'abcde'
+
+token = xor.create_token(KEY)
+bmsg = bytes(MSG, 'utf-8')
+print(f'bytes({MSG}: {bmsg}')
+encrypted = xor.xor_bin_with_token(bmsg, token)
+print(f'Key:{KEY} token:{token} encrypted:{encrypted}') 
+
+
+ip = "192.168.2.15"
+port = 2004
 
 cs = socket(AF_INET, SOCK_STREAM)
 cs.connect((ip, port))
 
 print("connected")
-cs.send("123456".encode("utf-8"))
-print("123456 send")
+#cs.send("123456".encode("utf-8"))
+cs.send(encrypted)
+print(f"encrypted sent: {encrypted}")
 
 
 data = cs.recv(1024)
-print("received: ", data.decode("utf-8"))
+print(f"received: data: {data}")
 cs.close()
 
 
--- sw/ex/socket/tcpserver.py
+++ sw/ex/socket/tcpserver.py
@@ -1,22 +1,25 @@
-from socket import *
+import socket
 
-host = "192.168.1.30"
-port = 5005
+host = "192.168.2.88"
+port = 2004
 
-ss = socket(AF_INET, SOCK_STREAM)
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ss.bind((host,port))
 ss.listen(1)
 print("listening...")
 
-conso, addr = ss.accept()
+conn, addr = ss.accept()
 print(str(addr), " connected")
 
-data = cs.recv(1024)
-print("data received: ", data.decode("utf-8"))
+data = conn.recv(1024)
+print(f"data received: data: {data}", )
 
-cs.send("from server".encode("utf-8"))
+print("\nReturning the same data to client")
+#conn.send("from server".encode("utf-8"))
+conn.send(data)
 print("message sent")
 
+conn.close()
 ss.close()
-
 
